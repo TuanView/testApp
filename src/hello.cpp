04133fa1f0b55579dd3185a41d2c9fe55c17235d
Tuan Doan <tuan.doan@viewglass.com>
2018-12-12 10:33:01 -0800

    HelloProj init

---
+++ src/hello.cpp
@@ -0,0 +1,11 @@
+#include <unistd.h>
+#include <stdio.h>
+
+using namespace std;
+
+int main(int ac, const char** av)
+{
+  printf("Hello from helloProj.\n");
+  // std::cout << "Hello from testProj." << std::endl;
+  return 0;
+}
